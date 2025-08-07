6fd80359f8a128c9d64c87c048d1d2d73acd35c6
Surbhi <135599566+surbhi150@users.noreply.github.com>
2025-08-08 00:19:21 +0530

    Update Total_Charts.py

--- Total_Charts.py
+++ Total_Charts.py
@@ -4,7 +4,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.figure_factory as ff
-from utils.load_data import load_data
+from load_data import load_data
 
 # Load the data
 df = load_data()
@@ -70,3 +70,4 @@ if show_chart:
 
 else:
     st.info("Click the button in the sidebar to display the charts.")
+
